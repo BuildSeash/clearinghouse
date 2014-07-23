dd3e3abdeab8142ffd695b0d9bcfa2124a478043
Chintan Choksi <chintan@Chintans-MacBook-Pro.local>
2014-07-23 13:58:50 -0400

    commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'clearinghouse',
+  packages = [''], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Seattle',
+  url = 'https://github.com/choksi81/clearinghouse', # use the URL to the github repo
+  download_url = 'https://github.com/choksi81/clearinghouse/tarball/0.1', # I'll explain this in a second
+  classifiers = [],
+)
